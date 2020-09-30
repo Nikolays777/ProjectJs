705fed4bb30ecb9bcaaa75b00896f1064ce9db42
CourseJ <boss.dudchak@gmail.com>
2020-09-30 16:54:29 +0300

    Lesson15 Practical

--- js/main.js
+++ js/main.js
@@ -1,22 +1,38 @@
 "use strict";
 
-let num = 50;
+const numberOfFilms = prompt('Количество', '');
+const personalMovieDB = {
+        count: numberOfFilms,
+        movies: {},
+        actors: {},
+        genres: [],
+        privat: false 
+      };
 
-// while (num <= 55) {
-//       console.log(num);
-//       num++;
-// }
 
-// do{
-//   console.log(num);
-//   num++;
-// }
 
-// while ( num < 55);
+for(let i = 0; i < 2; i++ ){
+    const a = prompt('Последний', ''),
+          b = +prompt('Оценка', '');
 
-for (let i = 1; i <= 8; i++) {
-    if (i === 6) {
-      continue;
+  if (a != null && b != null && a != '' && b != '' && a.length < 50){
+      personalMovieDB.movies[a] = b;
+      console.log('Good');
+    } else{
+      console.log('error');
+      i--;
     }
-    console.log(i);
 }
+
+if (personalMovieDB.count < 10){
+    console.log('Слишком мало фильмов');
+} else if(personalMovieDB.count >= 10 && personalMovieDB.count < 30){
+    console.log('Довольно много фильмов');
+} else if (personalMovieDB.count > 30){
+    console.log('Киноман');
+}else{
+  console.log('error');
+}
+
+console.log(personalMovieDB);
+
