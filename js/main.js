68fd97a2d24fef216145b4aa4d6f26b8d07be25c
CourseJ <boss.dudchak@gmail.com>
2020-09-30 10:05:39 +0300

    Lesson12 Start Make Application

--- js/main.js
+++ js/main.js
@@ -1,2 +1,19 @@
 "use strict";
-const number
+const numberOfFilms = +prompt('Сколько фильмов?', '');
+// console.log(numberOfFilms);
+const personalMovieDB = {
+  count: numberOfFilms,
+  movies: {},
+  actors: {},
+  general: [],
+  privat: false
+};
+const a = prompt('Последний', ''),
+      b = prompt('Оценка', ''),
+      c = prompt('Последний', ''),
+      d = prompt('Оценка', '');
+
+personalMovieDB.movies[a] = b;
+personalMovieDB.movies[c] = d;
+
+console.log(personalMovieDB);
